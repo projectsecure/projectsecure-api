834989f095dd002337b1b55dd475c75e98843cd0
Niklas Riekenbrauck <nikriek@users.noreply.github.com>
2017-01-28 15:31:37 +0100

    Update test_views.py

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -1,6 +1,6 @@
 from rest_framework.test import APITestCase
 from django.core.urlresolvers import reverse
-from rest_framework import status
+from rest_framework.status import HTTP_204_NO_CONTENT, HTTP_200_OK, HTTP_401_UNAUTHORIZED, HTTP_400_BAD_REQUEST
 from users.tests.factories import UserFactory, DEFAULT_PASSWORD
 from users.models import User
 import datetime
@@ -23,14 +23,14 @@ class TestUserViewSet(APITestCase):
         self.client.force_authenticate(user=user)
         response = self.client.get(reverse('user-me'))
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, HTTP_200_OK)
         self.assertEqual(response.json(), {'username': user.username,
                                            'color': user.color, 'email': user.email})
 
     def test_get_me_unauthenticated(self):
         response = self.client.get(reverse('user-me'))
 
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.status_code, HTTP_401_UNAUTHORIZED)
         self.assertEqual(response.json(), {'detail': 'Authentication credentials were not provided.'})
 
     def test_register_user_with_valid_data(self):
@@ -42,7 +42,7 @@ class TestUserViewSet(APITestCase):
         }
         response = self.client.post(reverse('user-register'), data=data)
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, HTTP_200_OK)
         data.pop('password')
         self.assertEqual(response.json(), data)
 
@@ -56,7 +56,7 @@ class TestUserViewSet(APITestCase):
         }
         response = self.client.post(reverse('user-register'), data=data)
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, HTTP_400_BAD_REQUEST)
         self.assertEqual(response.json(),
                          {'username': ['A user with that username already exists.']})
 
@@ -64,7 +64,7 @@ class TestUserViewSet(APITestCase):
         data = {}
         response = self.client.post(reverse('user-register'), data=data)
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, HTTP_400_BAD_REQUEST)
         self.assertEqual(response.json(), {'color': ['This field is required.'],
                                            'password': ['This field is required.'],
                                            'username': ['This field is required.']})
@@ -74,14 +74,14 @@ class TestUserViewSet(APITestCase):
         self.assertEqual(User.objects.count(), 1)
         response = self.client.delete(reverse('user-me'))
 
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(response.status_code, HTTP_204_NO_CONTENT)
         self.assertEqual(response.content, b'')
         self.assertEqual(User.objects.count(), 0)
 
     def test_delete_me_unauthenticated(self):
         response = self.client.delete(reverse('user-me'))
 
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.status_code, HTTP_401_UNAUTHORIZED)
         self.assertEqual(response.json(), {'detail': 'Authentication credentials were not provided.'})
     
     
@@ -91,14 +91,14 @@ class TestAuthObtainJWTView(APITestCase):
         data = {'username': user.username, 'password': DEFAULT_PASSWORD}
         response = self.client.post(reverse('auth-jwt-obtain'), data=data)
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, HTTP_200_OK)
         self.assertTrue(response.json().get('token') is not None)
 
     def test_login_with_wrong_credentials(self):
         data = {'username': 'atotallyrandomusernmae', 'password': 'atotallyrandompassword'}
         response = self.client.post(reverse('auth-jwt-obtain'), data=data)
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, HTTP_400_BAD_REQUEST)
         self.assertEqual(response.json(),
                          {'non_field_errors': ['Unable to login with provided credentials.']})
 
@@ -110,14 +110,14 @@ class TestAuthRefreshJWTView(APITestCase):
         data = {'token': token}
         response = self.client.post(reverse('auth-jwt-refresh'), data=data)
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, HTTP_200_OK)
         self.assertTrue(response.json().get('token') is not None)
 
     def test_refresh_with_invalid_token(self):
         data = {'token': 'sdfanfajnfsnfd'}
         response = self.client.post(reverse('auth-jwt-refresh'), data=data)
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, HTTP_400_BAD_REQUEST)
         self.assertEqual(response.json(), {'non_field_errors': ['Error decoding signature.']})
 
 
@@ -128,14 +128,14 @@ class TestAuthVerifyJWTView(APITestCase):
         data = {'token': token}
         response = self.client.post(reverse('auth-jwt-verify'), data=data)
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, HTTP_200_OK)
         self.assertEqual(response.json(), {'token': token})
 
     def test_verify_with_invalid_token(self):
         data = {'token': 'sdfanfajnfsnfd'}
         response = self.client.post(reverse('auth-jwt-verify'), data=data)
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, HTTP_400_BAD_REQUEST)
         self.assertEqual(response.json(), {'non_field_errors': ['Error decoding signature.']})
 
     def test_verify_with_expired_token(self):
@@ -144,5 +144,5 @@ class TestAuthVerifyJWTView(APITestCase):
         data = {'token': token}
         response = self.client.post(reverse('auth-jwt-verify'), data=data)
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, HTTP_400_BAD_REQUEST)
         self.assertEqual(response.json(), {'non_field_errors': ['Signature has expired.']})
